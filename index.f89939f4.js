f9f01fbf26829406b48f185abca65f9ca169742d
H-Maksym <105509906+H-Maksym@users.noreply.github.com>
2022-07-06 01:00:47 +0000

    Deploying to gh-pages from @ H-Maksym/team-project-Avengers@26a082e8b41d48e2799b43320bfb5c7950651835 🚀

--- index.f89939f4.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-franch-open]"),closeModalBtn:document.querySelector("[data-franch-close]"),modal:document.querySelector("[data-franch]")};function o(){e.modal.classList.toggle("is-hidd")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.f89939f4.js.map
